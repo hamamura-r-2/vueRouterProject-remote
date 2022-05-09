7c1a7d03c9f6129e55c124c9a7492b992428231f
hamamura1989 <r.hamamura1227@gmail.com>
2022-05-09 22:36:01 +0900

    vueRouter導入

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>This is About.</h2>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>This is Home.</h2>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
